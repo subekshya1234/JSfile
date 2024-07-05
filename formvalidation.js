ff19951b637fcce8f6c23e74de0ad19d2c8daf33
Sadichhyamhzn <sadikshyamhj@gmail.com>
2024-07-05 12:24:38 +0545

    Add files via upload

--- formvalidation.js
+++ formvalidation.js
@@ -16,8 +16,8 @@ function fnamevalidate() {
         return false
     }
 
-    else{
-        displaymsg('first name is valid','fnameMsg','green')
+    else {
+        displaymsg('first name is valid', 'fnameMsg', 'green')
         return true
     }
 }
@@ -35,8 +35,8 @@ function Lnamevalidate() {
         return false
     }
 
-    else{
-        displaymsg('Last Name is valid','LnameMsg','green')
+    else {
+        displaymsg('Last Name is valid', 'LnameMsg', 'green')
         return true
     }
 }
@@ -48,32 +48,65 @@ function emailvalidate() {
         displaymsg('email is mandotory', 'emailMsg', 'red')
     }
 
-    else if(!input.match(/^([a-zA-Z])[a-zA-Z0-9\-\.\_]+\@+([a-zA-Z])+\.+([a-z])$/)){
-        displaymsg('someone@123.gmail.com','emailMsg','blue')
+    else if (!input.match(/^([a-zA-Z])[a-zA-Z0-9\-\.\_]+\@+([a-zA-Z])+\.+([a-z])$/)) {
+        displaymsg('someone@123.gmail.com', 'emailMsg', 'blue')
     }
 
 
-    else{
-        displaymsg('email is valid','emailMsg','green')
+    else {
+        displaymsg('email is valid', 'emailMsg', 'green')
         return true
     }
 }
 
-function Passwordvalidate() {
+function pwvalidate() {
     let input = document.getElementById('Password').value
     if (input == '') {
 
-        displaymsg('Passwordf is mandotory', 'PasswordMsg', 'red')
+        displaymsg('Password is mandotory', 'PasswordMsg', 'red')
+        return false
     }
 
-    else if(!input.match(/(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$*!?]).{8,}$/)){
-        displaymsg('someone@123.gmail.com','emailMsg','blue')
-    }     
+    else if (!input.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$*!?]).{8,}$/)) {
+        displaymsg('Password must match the condition', 'PasswordMsg', 'red')
+return false
 
+    }
 
-    else{
-        displaymsg('Password is valid','PasswordMsg','green')
+    else {
+        displaymsg('Password is valid', 'PasswordMsg', 'green')
         return true
+    }
+}
+
+
+function Cwvalidate() {
+    let password = document.getElementById('Password').value
+    let Cpassword = document.getElementById('Cpassword').value
+
+    if (Cpassword == '') {
+        displaymsg('confirm password is mandatory', 'Cpasswordmsg', 'red')
+        return false
+    }
+
+    else if (password != Cpassword) {
+        displaymsg('password and confirm must be same', 'Cpasswordmsg', 'red')
+        return false
+    }
+
+    else {
+        displaymsg('okay', 'Cpasswordmsg', 'green')
+
+    }
+
+    function submitform(){
+        if ( fnamevalidate()&&Lnamevalidate()&&emailvalidate()&&pwvalidate()&&Cwvalidate()){
+            return true
+        }
+        {
+            return false
+        }
+            
     }
 }
 
